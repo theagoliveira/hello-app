47d7726bf07e8a5dba3cdd25b4b8cfa8e96ba001
Thiago Cavalcante <thiago.kun@gmail.com>
2019-08-04 17:27:03 -0300

    Add actions and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: '¡hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
 end
